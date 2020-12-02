3e770216eb3ebd6ed08d57e52c0508f7a428ea8b
JE-Chen <zenmailman@gmail.com>
2020-12-02 16:14:53 +0800

    refactor and update
    refactor and update

---
+++ Test/Test.py
@@ -0,0 +1,13 @@
+import JELogSystem
+a = JELogSystem.Log_System()
+a.Set_BoardCast_Lv(0)
+a.Normal("0",Except=True)
+a.Debug("0",Except=True)
+a.Info("0",Except=True)
+a.Warning("0",Except=True)
+a.Error("0",Except=True)
+a.Critical("0",Except=True)
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as README:
+    long_description = README.read()
+
+setuptools.setup(
+    name="JELogSystem",
+    version="0.0.0.0.2",
+    author="JE-Chen",
+    author_email="zenmailman@gmail.com",
+    description="JE use LogSystem",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JE-Chen/Python_Log_System_JE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Win32 (MS Windows)",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Chinese (Traditional)",
+    ]
+)
+
+#sdist bdist_wheel
+#python -m twine upload dist/*
